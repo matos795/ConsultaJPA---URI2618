469a913e9b5e36a8e2367dab63652fdca4fdc38e
Alexandre Matos <matosalexandre795@gmail.com>
2025-09-22 19:56:07 -0300

    Project Created

---
+++ src/main/java/com/consultaJPA/uri2618/Uri2618Application.java
@@ -0,0 +1,13 @@
+package com.consultaJPA.uri2618;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2618Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2618Application.class, args);
+	}
+
+}
